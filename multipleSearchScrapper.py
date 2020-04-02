8c942a5fef88dfcd7b9f2fbe6360185d782ecba1
Rasty-90 <as2paine@gmail.com>
2020-04-02 19:08:29 +0300

    html structure and changes to mutliple search

--- multipleSearchScrapper.py
+++ multipleSearchScrapper.py
@@ -3,7 +3,7 @@ import requests
 import json
 #Use this scraper when scraping multiple products from a search
 
-shopOwner="aquario" #your own shop/your client's shop
+shopOwner="Aquario" #your own shop/your client's shop
 #Default search url is https://www.skroutz.gr/search?keyphrase=
 defaultUrl="https://www.skroutz.gr/search?keyphrase="
 key = "Sera Vipan 100ml"
@@ -13,18 +13,18 @@ content=BeautifulSoup(response.content,"html.parser")
 products = []
 
 for product in content.findAll('div', attrs={"class": "card-content"}):
+   
     productObject = {
         #use UTF-8 encoding to grab the greek characters, and UTF-8 decoding afterwards to get rid of the additional "b" character 
         #that spawns in front of every value after the encoding
         "product": product.find('a', attrs={"class": "js-sku-link"}).text.encode('utf-8').decode('utf-8'),
-        "price": product.find('a', attrs={"class": "js-sku-link product-link"}).text.encode('utf-8').decode('utf-8'),
-        "shop": product.find('button', attrs={"class": "shop icon js-shop-info-link"}).text.encode('utf-8').decode('utf-8'),
+        "price": product.find('a', attrs={"class": "js-sku-link sku-link"}).text.encode('utf-8').decode('utf-8'),
+        "shop": product.find('span', attrs={"class": "shop-count"}).text.encode('utf-8').decode('utf-8').partition(' ')[2],
     }
     products.append(productObject)
 
-# optional:save the results to a JSON file in order to handle the data later
-# with open('productData.json', 'w', encoding='utf8') as outfile:
-#     json.dump(products, outfile, ensure_ascii=False)
-
-if products[0]["shop"]!="aquario":
-    print ("ΔΕΝ ΕΙΜΑΣΤΕ ΠΡΩΤΟΙ")
+#optional:save the results to a JSON file in order to handle the data later
+if products[0]["shop"]!=shopOwner:
+    print ( "To προϊόν υπάρχει σε χαμηλότερη τιμή")
+else:
+    print ("Έχουμε την καλύτερη τιμή")
