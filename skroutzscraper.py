809001132d1e383678a6e4f1560e3a9a4fec1836
Rasty-90 <as2paine@gmail.com>
2020-04-13 19:14:30 +0300

    changed storage file to csv instead of json

--- skroutzscraper.py
+++ skroutzscraper.py
@@ -1,17 +1,18 @@
 from multipleSearchScrapper import multipleSearch
 from insideProductPageScrapper import singleSearch
-import json 
+
+import csv
 
 #static values
 shopOwner="Aquario" #your own shop/your client's shop
 
-with open('productList.json','r', encoding='utf8') as json_file: #reads list of products
-    data = json.load(json_file) 
-    productList = data
 
+with open('listprod.csv', newline='', encoding='utf8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    productList = reader
 
-for listing in productList:
-    if listing["searchType"]=="single":
-        print (singleSearch(shopOwner,listing))
-    else:
-        print(multipleSearch(shopOwner,listing))
+    for listing in productList:
+        if listing["searchType"]=="single":
+            print (singleSearch(shopOwner,listing))
+        else:
+            print(multipleSearch(shopOwner,listing))
